a320b43d3d02da5de701af13054efb84ac38d264
Philip Taylor - Laptop <philip.dan.taylor@gmail.com>
2023-12-07 11:25:33 -0600

    Correct Cube Pos

--- Task2/script.js
+++ Task2/script.js
@@ -30,13 +30,13 @@ import {
       camera.position.set(0, 0, 10);
       // create a geometry
       const geometry = new BoxGeometry(2, 2, 2);
-      cube.position.set(0, 0, -2);
+     
       // create a default (white) Basic material
       const material = new MeshBasicMaterial();
       
       // create a Mesh containing the geometry and material
       const cube = new Mesh(geometry, material);
-      
+        cube.position.set(0, 0, -2);
       // add the mesh to the scene
       scene.add(cube);
       
