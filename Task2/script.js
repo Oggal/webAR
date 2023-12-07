a1160dbfe2c894bce59890ad098f8043f449ec34
Philip Taylor - Laptop <philip.dan.taylor@gmail.com>
2023-12-07 09:12:51 -0600

    Pasting in an example, just testing at this point

--- Task2/script.js
+++ Task2/script.js
@@ -1,37 +1,57 @@
-import { 
-        PerspectiveCamera,
-        MeshBasicMaterial,
-        Mesh,
-        BoxGeometry,
-        Scene,
+import {
+        BoxBufferGeometry,
         Color,
-        WebGLRenderer
-        } from "three";
-
-const container = document.querySelector("#AR_container");
-
-const scene = new Scene();
-scene.background = new Color(0x00dbed);
-
-const camera = new PerspectiveCamera(70, window.devicePixelRatio, 0.01, 20);
-camera.position.set(0, 0, 0.1);
-camera.lookAt(0, 0, 0);
-scene.add(camera);
-
-const box_geo = new BoxGeometry(0.1, 0.1, 0.1);
-const box_mat = new MeshBasicMaterial({ color: 0x00ff00 });
-const box = new Mesh(box_geo, box_mat);
-box.position.set(0, 0, -0.5);
-scene.add(box);
-
-const renderer = new WebGLRenderer({
-        alpha: true,
-        preserveDrawingBuffer: true,
-        canvas: canvas,
-        context: gl
-});
-renderer.autoClear = false;a
-renderer.render(scene, camera)
-container.appendChild(renderer.domElement);
-
-
+        Mesh,
+        MeshBasicMaterial,
+        PerspectiveCamera,
+        Scene,
+        WebGLRenderer,
+      } from 'three';
+      
+      // Get a reference to the container element that will hold our scene
+      const container = document.querySelector('#scene-container');
+      
+      // create a Scene
+      const scene = new Scene();
+      
+      // Set the background color
+      scene.background = new Color('skyblue');
+      
+      // Create a camera
+      const fov = 35; // AKA Field of View
+      const aspect = container.clientWidth / container.clientHeight;
+      const near = 0.1; // the near clipping plane
+      const far = 100; // the far clipping plane
+      
+      const camera = new PerspectiveCamera(fov, aspect, near, far);
+      
+      // every object is initially created at ( 0, 0, 0 )
+      // move the camera back so we can view the scene
+      camera.position.set(0, 0, 10);
+      
+      // create a geometry
+      const geometry = new BoxBufferGeometry(2, 2, 2);
+      
+      // create a default (white) Basic material
+      const material = new MeshBasicMaterial();
+      
+      // create a Mesh containing the geometry and material
+      const cube = new Mesh(geometry, material);
+      
+      // add the mesh to the scene
+      scene.add(cube);
+      
+      // create the renderer
+      const renderer = new WebGLRenderer();
+      
+      // next, set the renderer to the same size as our container element
+      renderer.setSize(container.clientWidth, container.clientHeight);
+      
+      // finally, set the pixel ratio so that our scene will look good on HiDPI displays
+      renderer.setPixelRatio(window.devicePixelRatio);
+      
+      // add the automatically created <canvas> element to the page
+      container.append(renderer.domElement);
+      
+      // render, or 'create a still image', of the scene
+      renderer.render(scene, camera);
